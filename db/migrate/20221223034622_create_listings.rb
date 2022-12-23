69eef7930664c8ae04079c9b8cc38708c98e5ef4
dujota <denisdujota@gmail.com>
2022-12-23 10:29:05 -0500

    add db validation

--- db/migrate/20221223034622_create_listings.rb
+++ db/migrate/20221223034622_create_listings.rb
@@ -4,8 +4,8 @@ class CreateListings < ActiveRecord::Migration[7.0]
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :type, null: false, foreign_key: true
 
-      t.string :title, null: true, limit: 40, null: false
-      t.text :description, null: true, limit: 300, null: false
+      t.string :title, limit: 40, null: false
+      t.text :description, limit: 300, null: false
 
       t.timestamps
     end
