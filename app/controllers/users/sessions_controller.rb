54e89e45e045820141e9f8d58ffef87347c6ac4a
Denis S Dujota <denisdujota@gmail.com>
2022-12-27 02:25:18 -0500

    rmeove old comments

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,35 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
   respond_to :json
-  # def create
-  #   self.resource =
-  #     warden.authenticate!(auth_options.merge(store: !request.format.json?))
-  #   super
-  # end
 
   private
 
