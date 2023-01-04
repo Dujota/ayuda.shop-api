8545651d03292cbb0655140ba4fa9bcd12fd06e9
Denis S Dujota <denisdujota@gmail.com>
2023-01-04 09:56:14 -0500

    add listings by user and listings by user by type

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,12 +1,40 @@
 class API::V1::ListingsController < ApplicationController
   before_action :authenticate_user!, except: %i[index]
+  before_action :set_type, only: %i[my_listings]
   load_and_authorize_resource
 
   # GET /api/v1/listings
   def index
     @listings = Listing.all
-
     render json: @listings
+  end
+
+  def my_listings
+    if @type
+      @listings = Listing.user_listings(current_user).by_type(@type.id)
+      if @listings
+        render json: {
+                 status: 200,
+                 message: "Listings filtered by #{params[:type]}",
+                 data: @listings
+               },
+               status: :ok
+      else
+        user_listing_types_failed
+      end
+    else
+      @listings = Listing.user_listings(current_user)
+      if @listings
+        render json: {
+                 status: 200,
+                 message: "All listings for user #{current_user.id}",
+                 data: @listings
+               },
+               status: :ok
+      else
+        all_user_listings_failed
+      end
+    end
   end
 
   # GET /api/v1/listings/1
@@ -44,12 +72,32 @@ class API::V1::ListingsController < ApplicationController
   private
 
   # # Use callbacks to share common setup or constraints between actions.
-  # def set_listing
-  #   @listing = Listing.find(params[:id])
-  # end
+
+  def user_listing_types_failed
+    render json: { status: 400 }, status: :bad_request
+  end
+
+  def all_user_listings_failed
+    render json: {
+             status: {
+               message: "User listings couldn't be found successfully."
+             }
+           },
+           status: :unprocessable_entity
+  end
+
+  def set_type
+    @type = Type.find_by(tag: params[:type]) if params[:type]
+  end
 
   # Only allow a list of trusted parameters through.
   def listing_params
-    params.require(:listing).permit(:title, :description, :type_id, :user_id)
+    params.require(:listing).permit(
+      :title,
+      :description,
+      :type_id,
+      :user_id,
+      :type
+    )
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,4 +6,11 @@ class Listing < ApplicationRecord
   validates :title, uniqueness: { case_sensitive: false }
   validates :title, length: { minimum: 10, maximum: 40 }
   validates :description, length: { minimum: 50, maximum: 300 }
+
+  scope :user_listings, ->(user) { where(user_id: user.id) }
+  scope :by_type, ->(type) { where(type_id: type) }
+  scope :seeking, -> { where(type_id: 2) }
+  scope :offering, -> { where(type_id: 1) }
+
+  # Ex:- scope :active, -> {where(:active => true)}
 end
