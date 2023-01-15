2b2a053712d7acde89373c0a9f1072a4c3986583
Denis S Dujota <denisdujota@gmail.com>
2023-01-15 13:01:27 -0500

    add comment for future consideratiuon

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,6 +4,7 @@ class Conversation < ApplicationRecord
   has_many :users, through: :user_conversations
   has_many :messages, dependent: :destroy
 
+  # make sure that a conversation is created with a listing
   validates :listing, presence: true
   # make sure that a conversation between the same users and the same listing is not created twice.
   valdidates_uniqueness_of :listing_id, scope: :users
